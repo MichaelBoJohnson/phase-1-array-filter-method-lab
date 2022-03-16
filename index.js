81f7c9a5e6c77e6a62997c512c7604e2763e2b8d
Michael Johnson <michaelbojohnson@gmail.com>
2022-03-16 16:09:18 -0500

    commit tree 48d4117fe026ddbb81796e9d54cf5bec1b5cd0da

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name){
+  let user = drivers.filter(element => name === element || name.toLowerCase() === element);
+        return user;
+    };
+
+function fuzzyMatch (drivers, name){
+    let user = drivers.filter(element => name.slice(0,2) === element.slice(0,2));
+    return user;
+}
+
+
+function matchName (drivers, name){
+    let user = drivers.filter(element => name === element.name);
+    return user;
+}
+
+
